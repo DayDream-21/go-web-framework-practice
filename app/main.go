be89e4f70ea3f93755a06237f4542f468b50e627
Slava Mashkov <mashkov.vd@gmail.com>
2024-05-25 01:23:46 +0300

    Initialize server and render user profile page

---
+++ app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	slick "github.com/DayDream-21/big-web-framework"
+	"github.com/DayDream-21/big-web-framework/app/views/profile"
+	"log/slog"
+)
+
+func main() {
+	app := slick.New()
+
+	app.Get("/profile", HandleUserProfile)
+
+	slog.Info("starting server on port :3000")
+	err := app.Start(":3000")
+	if err != nil {
+		slog.Error("error starting server", "err", err)
+	}
+}
+
+func HandleUserProfile(c *slick.Context) error {
+	return c.Render(profile.Index())
+}
