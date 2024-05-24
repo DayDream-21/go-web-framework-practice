95f6567309cd9154068ae9ed0aac33ef0ef35238
Slava Mashkov <mashkov.vd@gmail.com>
2024-05-25 02:37:07 +0300

    Set style location for index page

--- src/app/views/profile/index_templ.go
+++ src/app/views/profile/index_templ.go
@@ -23,7 +23,7 @@ func Index() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h1>User profile page</h1>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"../../../public/output.css\"><title>User profile</title></head><body class=\"bg-gray-600\"><h1 class=\"text-lime-500\">User profile page</h1></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
