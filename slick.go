2ad3921b313ec4b31aa39575dc24f6f6781a6401
Slava Mashkov <mashkov.vd@gmail.com>
2024-05-25 02:38:18 +0300

    Serve static files and change their location

--- slick.go
+++ slick.go
@@ -35,6 +35,8 @@ func New() *Slick {
 }
 
 func (s *Slick) Start(port string) error {
+	s.router.ServeFiles("/public/*filepath", http.Dir("./src/public"))
+
 	return http.ListenAndServe(port, s.router)
 }
 
